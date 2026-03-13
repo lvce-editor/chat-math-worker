ab4465368090f9cdeb0b2193b90482f98821210f
Le Vivilet <levivilet@pm.me>
2026-03-13 15:31:11 +0100

    add fns

--- packages/chat-math-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-math-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,18 +1,10 @@
 import * as CoordinatorCommands from '../CoordinatorCommands/CoordinatorCommands.ts'
+import { getMathBlockDom, getMathInlineDom } from '../GetMathDom/GetMathDom.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import * as NetworkCommandMap from './NetworkCommandMap.ts'
 
 export const commandMap = {
-  ...NetworkCommandMap.networkCommandMap,
-  'chatMath.cancelRun': CoordinatorCommands.cancelRun,
-  'chatMath.consumeEvents': CoordinatorCommands.consumeEvents,
-  'chatMath.createSession': CoordinatorCommands.createSession,
-  'chatMath.deleteSession': CoordinatorCommands.deleteSession,
-  'chatMath.getSession': CoordinatorCommands.getSession,
-  'chatMath.listSessions': CoordinatorCommands.listSessions,
-  'chatMath.submit': CoordinatorCommands.submit,
-  'chatMath.subscribe': CoordinatorCommands.subscribe,
-  'chatMath.unsubscribe': CoordinatorCommands.unsubscribe,
-  'chatMath.waitForEvents': CoordinatorCommands.waitForEvents,
+  'ChatMath.getMathBlockDom': getMathBlockDom,
+  'chatMath.getMathInlineDom': getMathInlineDom,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
