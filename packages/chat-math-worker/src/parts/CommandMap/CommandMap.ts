022e0834c1a5b6f19e375a1e453c6f37d83471ba
Le Vivilet <levivilet@pm.me>
2026-03-13 15:31:27 +0100

    unused

--- packages/chat-math-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-math-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,5 @@
-import * as CoordinatorCommands from '../CoordinatorCommands/CoordinatorCommands.ts'
 import { getMathBlockDom, getMathInlineDom } from '../GetMathDom/GetMathDom.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
-import * as NetworkCommandMap from './NetworkCommandMap.ts'
 
 export const commandMap = {
   'ChatMath.getMathBlockDom': getMathBlockDom,
