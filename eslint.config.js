6811c60442d1aaa2888200783827f4513e6cb5a9
Le Vivilet <levivilet@pm.me>
2026-08-01 06:12:46 -0700

    feature: update eslint dependencies (#25)

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 import * as regex from '@lvce-editor/eslint-plugin-regex'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
-  ...actions.default,
+  ...config.recommendedActions,
   ...tsconfig.default,
   ...regex.default,
   {
@@ -26,4 +26,4 @@ export default [
       'virtual-dom/secure-links': 'off',
     },
   },
-]
+])
